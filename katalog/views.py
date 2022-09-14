4b854788d450c9deaf638813e92c19469cf6114a
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2022-09-14 14:50:38 +0700

    tugas 2 pbp

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Christopher Nathanael Wijaya',
+        'student_id': '2106630044'
+    }
+    return render(request, "katalog.html", context)
